3b6bfb560a6fc076dee6b1e28f0a1e3efa2a6116
MariaSyrokvash <mariya.syrokvash@gmail.com>
2021-03-12 15:32:06 +0300

    feat: add menu

--- src/ui/01_app/v2-Header/Header.tsx
+++ src/ui/01_app/v2-Header/Header.tsx
@@ -14,62 +14,52 @@ export const Header: React.FC<MenuType> = ({theme, toggleTheme}) => {
 
 	return (
 		<>
-				<div className={style.overlay}>
-					<label htmlFor="toggle" className={style.label}></label>
+			<div className={`${style.overlay} ${theme === 'light' ? style.overlayLight : style.overlayDark}` }>
+				<label htmlFor="toggle" className={style.label}></label>
+			</div>
+			<input type="checkbox" id="toggle" name="toggle" className={style.toggle}/>
+		<nav
+			className={`${style.navBox} ${theme === 'light' ? style.light : style.dark} ${style.verticalNav} `}>
+			<ul className={style.navList}>
+				<div className={style.navListInner}>
+					<li className={style.navItem}>
+						<NavLink activeClassName={style.selected}
+										 className={`${style.link} ${theme === 'dark' ? style.dark : style.light}`}
+										 to={PATH.REGISTRATION}>Sign up</NavLink>
+					</li>
+					<li className={style.navItem}>
+						<NavLink className={`${style.link} ${theme === 'dark' ? style.dark : style.light}`}
+										 activeClassName={style.selected} to={PATH.LOGIN}>Sign in</NavLink>
+					</li>
+					<li className={style.navItem}>
+						<NavLink className={`${style.link} ${theme === 'dark' ? style.dark : style.light}`}
+										 activeClassName={style.selected} to={PATH.PROFILE}>Profile</NavLink>
+					</li>
+					{/*<li className={style.navItem}>*/}
+					{/*	<NavLink className={`${style.link} ${theme === 'dark' ? style.dark : style.light}`}*/}
+					{/*					 activeClassName={style.selected} to={PATH.RECOVERY_PASSWORD}>Recovery Password</NavLink>*/}
+					{/*</li>*/}
+					{/*<li className={style.navItem}>*/}
+					{/*	<NavLink className={`${style.link} ${theme === 'dark' ? style.dark : style.light}`}*/}
+					{/*					 activeClassName={style.selected} to={PATH.NEW_PASSWORD}>New Password</NavLink>*/}
+					{/*</li>*/}
+
+					<li className={style.navItem}>
+						<NavLink className={`${style.link} ${theme === 'dark' ? style.dark : style.light}`}
+										 activeClassName={style.selected} to={PATH.PACKS}>Packs</NavLink>
+					</li>
 				</div>
-
-				<input type="checkbox" id="toggle" name="toggle" className={style.toggle}/>
-					<div className={style.verticalNav}>
-						<a href="/">NEWS</a>
-						<a href="/">ABOUT</a>
-						<a href="/">BLOG</a>
-						<a href="/">CONTACT</a>
-					</div>
-
+				<div className={style.navListInnerSecond}>
+					<li className={style.navItem}>
+						<Logout/>
+					</li>
+					<li className={`${style.navItemTheme} ${style.navItem}`}>
+						<Theme theme={theme} toggleTheme={toggleTheme}/>
+					</li>
+				</div>
+			</ul>
+		</nav>
 			<div className={style.puppies}></div>
-
-
-		{/*<nav*/}
-		{/*	className={`${style.navBox} ${theme === 'light' ? style.light : style.dark} `}>*/}
-		{/*	<ul className={style.navList}>*/}
-		{/*		<div className={style.navListInner}>*/}
-		{/*			<li className={style.navItem}>*/}
-		{/*				<NavLink activeClassName={style.selected}*/}
-		{/*								 className={`${style.link} ${theme === 'dark' ? style.dark : style.light}`}*/}
-		{/*								 to={PATH.REGISTRATION}>Sign up</NavLink>*/}
-		{/*			</li>*/}
-		{/*			<li className={style.navItem}>*/}
-		{/*				<NavLink className={`${style.link} ${theme === 'dark' ? style.dark : style.light}`}*/}
-		{/*								 activeClassName={style.selected} to={PATH.LOGIN}>Sign in</NavLink>*/}
-		{/*			</li>*/}
-		{/*			<li className={style.navItem}>*/}
-		{/*				<NavLink className={`${style.link} ${theme === 'dark' ? style.dark : style.light}`}*/}
-		{/*								 activeClassName={style.selected} to={PATH.PROFILE}>Profile</NavLink>*/}
-		{/*			</li>*/}
-		{/*			/!*<li className={style.navItem}>*!/*/}
-		{/*			/!*	<NavLink className={`${style.link} ${theme === 'dark' ? style.dark : style.light}`}*!/*/}
-		{/*			/!*					 activeClassName={style.selected} to={PATH.RECOVERY_PASSWORD}>Recovery Password</NavLink>*!/*/}
-		{/*			/!*</li>*!/*/}
-		{/*			/!*<li className={style.navItem}>*!/*/}
-		{/*			/!*	<NavLink className={`${style.link} ${theme === 'dark' ? style.dark : style.light}`}*!/*/}
-		{/*			/!*					 activeClassName={style.selected} to={PATH.NEW_PASSWORD}>New Password</NavLink>*!/*/}
-		{/*			/!*</li>*!/*/}
-
-		{/*			<li className={style.navItem}>*/}
-		{/*				<NavLink className={`${style.link} ${theme === 'dark' ? style.dark : style.light}`}*/}
-		{/*								 activeClassName={style.selected} to={PATH.PACKS}>Packs</NavLink>*/}
-		{/*			</li>*/}
-		{/*		</div>*/}
-		{/*		<div className={style.navListInnerSecond}>*/}
-		{/*			<li className={style.navItem}>*/}
-		{/*				<Logout/>*/}
-		{/*			</li>*/}
-		{/*			<li className={`${style.navItemTheme} ${style.navItem}`}>*/}
-		{/*				<Theme theme={theme} toggleTheme={toggleTheme}/>*/}
-		{/*			</li>*/}
-		{/*		</div>*/}
-		{/*	</ul>*/}
-		{/*</nav>*/}
 		</>
 	)
 }
